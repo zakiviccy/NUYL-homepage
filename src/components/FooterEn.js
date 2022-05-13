1d3a4d070dc4a75d3e29906c6a00b32b73c789c5
Takaya-Yamazato <yamazato@ilas.nagoya-u.ac.jp>
2022-05-13 19:35:43 +0900

    5/12

--- src/components/FooterEn.js
+++ src/components/FooterEn.js
@@ -1,23 +1,17 @@
-import React from 'react'
-import {
-  FaEnvelope,
-  FaSubway,
-  FaUserFriends,
-  FaVimeoV,
-  FaHome,
-} from 'react-icons/fa'
-import { GiJapan } from 'react-icons/gi'
+import React from "react";
+import { FaEnvelope, FaSubway, FaUserFriends, FaVimeoV, FaHome } from "react-icons/fa";
+import { GiJapan } from "react-icons/gi";
 // import { RiEnglishInput } from "react-icons/ri";
-import { ImBooks } from 'react-icons/im'
+import { ImBooks } from "react-icons/im";
 // import { VscCircuitBoard } from "react-icons/vsc";
-import { GoCircuitBoard } from 'react-icons/go'
-import { MdAnnouncement } from 'react-icons/md'
+import { GoCircuitBoard } from "react-icons/go";
+import { MdAnnouncement } from "react-icons/md";
 
 // import { FaEnvelope } from "react-icons/fa";
 // import { FaBeer } from "@react-icons/all-files/fa/FaBeer";
 
-import { Link } from 'gatsby'
-import { StaticImage } from 'gatsby-plugin-image'
+import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 // import logo from "../img/og-image.jpg";
 // import logo from '../img/logo.png'
 // import facebook from '../img/social/facebook.svg'
@@ -27,20 +21,15 @@ import { StaticImage } from 'gatsby-plugin-image'
 // import youtube from '../img/social/youtube.svg'
 // import github from "../img/github-icon.svg";
 
-const width = 300
+const width = 300;
 
 const Footer = class extends React.Component {
   render() {
     return (
       <footer className="footer has-background-black has-text-white-ter">
         <div className="content has-text-centered">
-          <Link to="/">
-            <StaticImage
-              src="../img/logo.png"
-              width={width}
-              alt="Yamazato Laboratory"
-              placeholder="blurred"
-            />
+          <Link to="/en/">
+            <StaticImage src="../img/logo.png" width={width} alt="Yamazato Laboratory" placeholder="blurred" />
           </Link>
         </div>
         <div className="content has-text-centered has-background-info has-text-danger">
@@ -62,41 +51,25 @@ const Footer = class extends React.Component {
                       </Link>
                     </li>
                     <li>
-                      <Link
-                        className="navbar-item"
-                        to="/en/research/"
-                        title="Reseach"
-                      >
+                      <Link className="navbar-item" to="/en/research/" title="Reseach">
                         <GoCircuitBoard />
                         &nbsp;&nbsp;Research
                       </Link>
                     </li>
                     <li>
-                      <Link
-                        className="navbar-item"
-                        to="/en/publications/"
-                        title="Publications"
-                      >
+                      <Link className="navbar-item" to="/en/publications/" title="Publications">
                         <ImBooks />
                         &nbsp;&nbsp;Publications
                       </Link>
                     </li>
                     <li>
-                      <Link
-                        className="navbar-item"
-                        to="/en/#contact"
-                        title="Contact"
-                      >
+                      <Link className="navbar-item" to="/en/#contact" title="Contact">
                         <FaEnvelope />
                         &nbsp;&nbsp;Contact
                       </Link>
                     </li>
                     <li>
-                      <Link
-                        className="navbar-item"
-                        to="/"
-                        title="go to Japanese page"
-                      >
+                      <Link className="navbar-item" to="/" title="go to Japanese page">
                         <GiJapan />
                         &nbsp;&nbsp;Japanese
                       </Link>
@@ -108,48 +81,22 @@ const Footer = class extends React.Component {
                 <section>
                   <ul className="menu-list">
                     <li>
-                      <a
-                        className="navbar-item"
-                        href="https://en.nagoya-u.ac.jp/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        title="Nagoya University"
-                      >
+                      <a className="navbar-item" href="https://en.nagoya-u.ac.jp/" target="_blank" rel="noopener noreferrer" title="Nagoya University">
                         Nagoya University
                       </a>
                     </li>
                     <li>
-                      <a
-                        className="navbar-item"
-                        href="https://www.ilas.nagoya-u.ac.jp/en/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        title="Institute of Liberal Arts and Sciences, Nagoya University"
-                      >
-                        Institute of Liberal Arts and Sciences, Nagoya
-                        University
+                      <a className="navbar-item" href="https://www.ilas.nagoya-u.ac.jp/en/" target="_blank" rel="noopener noreferrer" title="Institute of Liberal Arts and Sciences, Nagoya University">
+                        Institute of Liberal Arts and Sciences, Nagoya University
                       </a>
                     </li>
                     <li>
-                      <a
-                        className="navbar-item"
-                        href="http://www.nuee.nagoya-u.ac.jp/index-e.php"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        title="Department of Information and Communication Engineering, Graduate School of Engineering, Nagoya University"
-                      >
-                        Department of Information and Communication Engineering,
-                        Nagoya University
+                      <a className="navbar-item" href="http://www.nuee.nagoya-u.ac.jp/index-e.php" target="_blank" rel="noopener noreferrer" title="Department of Information and Communication Engineering, Graduate School of Engineering, Nagoya University">
+                        Department of Information and Communication Engineering, Nagoya University
                       </a>
                     </li>
                     <li>
-                      <a
-                        className="navbar-item"
-                        href="https://www.katayama.nuee.nagoya-u.ac.jp/index-e.php"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        title="Nagoya University Katayama Laboratory"
-                      >
+                      <a className="navbar-item" href="https://www.katayama.nuee.nagoya-u.ac.jp/index-e.php" target="_blank" rel="noopener noreferrer" title="Nagoya University Katayama Laboratory">
                         Nagoya University Katayama Laboratory
                       </a>
                     </li>
@@ -181,13 +128,7 @@ const Footer = class extends React.Component {
                   <Link to="/en/#access" title="Access">
                     <FaSubway />
                   </Link>
-                  <a
-                    className="navbar-item"
-                    href="https://vimeo.com/user96502067"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    title="Vimeo: Takaya Yamazato"
-                  >
+                  <a className="navbar-item" href="https://vimeo.com/user96502067" target="_blank" rel="noopener noreferrer" title="Vimeo: Takaya Yamazato">
                     <FaVimeoV />
                   </a>
                 </div>
@@ -196,8 +137,8 @@ const Footer = class extends React.Component {
           </div>
         </div>
       </footer>
-    )
+    );
   }
-}
+};
 
-export default Footer
+export default Footer;
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from "react";
 // import { graphql } from "gatsby"
-import Layout from '../components/Layout'
+import Layout from "../components/Layout";
 
 const NotFoundPage = () => (
   <Layout>
@@ -9,18 +9,14 @@ const NotFoundPage = () => (
         <div className="columns">
           <div className="column is-10 is-offset-1">
             <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                NOT FOUND
-              </h2>
-              <h5>
-                You just hit a route that doesn&#39;t exist... the sadness.
-              </h5>
+              <h2 className="is-size-2 has-text-weight-bold">NOT FOUND</h2>
+              <h5>You just hit a route that doesn&#39;t exist... the sadness.</h5>
             </div>
           </div>
         </div>
       </div>
     </section>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
