3eac6e0549f1db68164a8435f5dfbe6e23578875
Takaya-Yamazato <yamazato@ilas.nagoya-u.ac.jp>
2022-05-18 20:35:20 +0900

    修正

--- src/pages/en/news/index.js
+++ src/pages/en/news/index.js
@@ -1,7 +1,7 @@
-import * as React from 'react'
+import * as React from "react";
 // import { graphql } from "gatsby"
-import Layout from '../../../components/LayoutEnWhite'
-import BlogRoll from '../../../components/BlogRollEn'
+import Layout from "../../../components/LayoutEnWhite";
+import BlogRoll from "../../../components/BlogRollEn";
 
 export default class BlogIndexPage extends React.Component {
   render() {
@@ -11,7 +11,7 @@ export default class BlogIndexPage extends React.Component {
           className="full-width-image margin-top-0"
           style={{
             // backgroundImage: `url('../../img/ob-image.jpg')`,
-            backgroundImage: `url('../../../img/home-jumbotron.jpg')`, // これはダメ
+            backgroundImage: `url('../../img/home-jumbotron.jpg')`, // これはダメ
             // backgroundImage: {data.markdownRemark.image},
             backgroundPosition: `top left`,
             backgroundAttachment: `fixed`,
@@ -20,10 +20,10 @@ export default class BlogIndexPage extends React.Component {
           <h1
             className="has-text-weight-bold is-size-2"
             style={{
-              boxShadow: '0.5rem 0 0 #002c76, -0.5rem 0 0 #002c76',
-              backgroundColor: '#002c76',
-              color: 'white',
-              padding: '1rem',
+              boxShadow: "0.5rem 0 0 #002c76, -0.5rem 0 0 #002c76",
+              backgroundColor: "#002c76",
+              color: "white",
+              padding: "1rem",
             }}
           >
             News and Updates
@@ -38,6 +38,6 @@ export default class BlogIndexPage extends React.Component {
           </div>
         </div>
       </Layout>
-    )
+    );
   }
 }
