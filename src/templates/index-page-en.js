bc01f1bf549ab8809fb6ce7f7b1047eac3d0e31c
Takaya-Yamazato <yamazato@ilas.nagoya-u.ac.jp>
2022-07-08 21:57:16 +0900

    7/7

--- src/templates/index-page-en.js
+++ src/templates/index-page-en.js
@@ -1,35 +1,27 @@
-import * as React from 'react'
-import PageTopEn from '../components/PageTopEn'
+import * as React from "react";
+import PageTopEn from "../components/PageTopEn";
 // import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
-import SeO from '../components/seo'
-import Layout from '../components/LayoutEn'
-import BlogRoll from '../components/indexRollEn'
-import { StaticImage } from 'gatsby-plugin-image'
-import { FaUserTie } from '@react-icons/all-files/fa/FaUserTie'
-import { FaUserGraduate } from '@react-icons/all-files/fa/FaUserGraduate'
-import { FaUserFriends } from '@react-icons/all-files/fa/FaUserFriends'
-import { FaPhoneAlt } from '@react-icons/all-files/fa/FaPhoneAlt'
-import { FaEnvelope } from '@react-icons/all-files/fa/FaEnvelope'
-import { FaSubway } from '@react-icons/all-files/fa/FaSubway'
-import { SiGooglemaps } from '@react-icons/all-files/si/SiGooglemaps'
+import { Link, graphql } from "gatsby";
+import SeO from "../components/seo";
+import Layout from "../components/LayoutEn";
+import BlogRoll from "../components/indexRollEn";
+import { StaticImage } from "gatsby-plugin-image";
+import { FaUserTie } from "@react-icons/all-files/fa/FaUserTie";
+import { FaUserGraduate } from "@react-icons/all-files/fa/FaUserGraduate";
+import { FaUserFriends } from "@react-icons/all-files/fa/FaUserFriends";
+import { FaPhoneAlt } from "@react-icons/all-files/fa/FaPhoneAlt";
+import { FaEnvelope } from "@react-icons/all-files/fa/FaEnvelope";
+import { FaSubway } from "@react-icons/all-files/fa/FaSubway";
+import { SiGooglemaps } from "@react-icons/all-files/si/SiGooglemaps";
 
-const width = 300
+const width = 300;
 
 const IndexPageTemplateEN = ({ data }) => (
   <Layout>
-    <SeO
-      title={data.markdownRemark.frontmatter.title}
-      description={data.markdownRemark.frontmatter.description}
-    />
+    <SeO title={data.markdownRemark.frontmatter.title} description={data.markdownRemark.frontmatter.description} />
     <div className="hero-body">
       <div className="container has-text-centered">
-        <StaticImage
-          src="../img/logo.png"
-          width={width}
-          alt="Nagoya University Yamazato Laboratory"
-          placeholder="blurred"
-        />
+        <StaticImage src="../img/logo.png" width={width} alt="Nagoya University Yamazato Laboratory" placeholder="blurred" />
       </div>
     </div>
     <section className="blog-posts">
@@ -40,27 +32,15 @@ const IndexPageTemplateEN = ({ data }) => (
               <div className="column is-12 post">
                 <article className="columns featured">
                   <div className="column is-7 post-img ">
-                    <StaticImage
-                      src="../img/ITS-VLC.png"
-                      alt="ITS-VLC"
-                      placeholder="blurred"
-                    />
+                    <StaticImage src="../img/ITS-VLC.png" alt="ITS-VLC" placeholder="blurred" />
                   </div>
                   <div className="column is-5 featured-content va">
                     <div>
                       <h1 className="heading post-category">Research</h1>
-                      <h1 className="title post-title">
-                        Image sensor communication
-                      </h1>
-                      <p className="post-excerpt">
-                        We conduct research on image-sensor communication (ISC),
-                        one of the visible light communication (VLC) system.
-                      </p>
+                      <h1 className="title post-title">Image sensor communication</h1>
+                      <p className="post-excerpt">We conduct research on image-sensor communication (ISC), one of the visible light communication (VLC) system.</p>
                       <br />
-                      <Link
-                        className="button"
-                        to="/en/Image-sensor-communication/"
-                      >
+                      <Link className="btn" to="/en/Image-sensor-communication/">
                         Read More
                       </Link>
                     </div>
@@ -76,28 +56,16 @@ const IndexPageTemplateEN = ({ data }) => (
                   <div className="column is-5 featured-content va">
                     <div>
                       <h1 className="heading post-category">Research</h1>
-                      <h1 className="title post-title">
-                        Stochastic resonance for communication
-                      </h1>
-                      <p className="post-excerpt">
-                        Stochastic resonance (SR) is a nonlinear phenomenon in
-                        which a system response is enhanced by additional noise.
-                      </p>
+                      <h1 className="title post-title">Stochastic resonance for communication</h1>
+                      <p className="post-excerpt">Stochastic resonance (SR) is a nonlinear phenomenon in which a system response is enhanced by additional noise.</p>
                       <br />
-                      <Link
-                        className="button"
-                        to="/en/Sub-threshold-signal-detection-using-stochastic-resonance/"
-                      >
+                      <Link className="btn" to="/en/Sub-threshold-signal-detection-using-stochastic-resonance/">
                         Read more
                       </Link>
                     </div>
                   </div>
                   <div className="column is-7 post-img ">
-                    <StaticImage
-                      src="../img/SR.png"
-                      alt="Stochastic Resonance"
-                      placeholder="blurred"
-                    />
+                    <StaticImage src="../img/SR.png" alt="Stochastic Resonance" placeholder="blurred" />
                   </div>
                 </article>
               </div>
@@ -108,24 +76,15 @@ const IndexPageTemplateEN = ({ data }) => (
               <div className="column is-12 post">
                 <article className="columns featured">
                   <div className="column is-7 post-img ">
-                    <StaticImage
-                      src="../img/USA.png"
-                      alt="Ultrasonic sensor array"
-                      placeholder="blurred"
-                    />
+                    <StaticImage src="../img/USA.png" alt="Ultrasonic sensor array" placeholder="blurred" />
                   </div>
                   <div className="column is-5 featured-content va">
                     <div>
                       <h1 className="heading post-category">Research</h1>
-                      <h1 className="title post-title">
-                        Ultrasonic sensor array
-                      </h1>
-                      <p className="post-excerpt">
-                        We are working on an ultrasonic sensor array system for
-                        slow-moving vehicles.
-                      </p>
+                      <h1 className="title post-title">Ultrasonic sensor array</h1>
+                      <p className="post-excerpt">We are working on an ultrasonic sensor array system for slow-moving vehicles.</p>
                       <br />
-                      <Link to="/en/research/" className="button">
+                      <Link to="/en/research/" className="btn">
                         Read More
                       </Link>
                     </div>
@@ -139,25 +98,14 @@ const IndexPageTemplateEN = ({ data }) => (
               <div className="column post is-6">
                 <article className="columns is-multiline">
                   <div className="column is-12 post-img">
-                    <StaticImage
-                      src="../img/Rotary-LED.png"
-                      alt="ITS-VLC"
-                      placeholder="blurred"
-                    />
+                    <StaticImage src="../img/Rotary-LED.png" alt="ITS-VLC" placeholder="blurred" />
                   </div>
                   <div className="column is-12 featured-content ">
                     <h1 className="heading post-category">Research</h1>
                     <h1 className="title post-title">Rotary LED transmitter</h1>
-                    <p className="post-excerpt">
-                      To improve the data rate of image sensor communication
-                      (ISC), we are currently working on using the afterimage
-                      effect by a rotary LED transmitter.
-                    </p>
+                    <p className="post-excerpt">To improve the data rate of image sensor communication (ISC), we are currently working on using the afterimage effect by a rotary LED transmitter.</p>
                     <br />
-                    <Link
-                      to="/en/Image-sensor-communication/"
-                      className="button"
-                    >
+                    <Link to="/en/Image-sensor-communication/" className="btn">
                       Read More
                     </Link>
                   </div>
@@ -166,27 +114,14 @@ const IndexPageTemplateEN = ({ data }) => (
               <div className="column post is-6">
                 <article className="columns is-multiline">
                   <div className="column is-12 post-img">
-                    <StaticImage
-                      src="../img/Global_Open_Educational_Resources_Logo.svg"
-                      alt="Open Educational Resources Logo"
-                      placeholder="blurred"
-                    />
+                    <StaticImage src="../img/Global_Open_Educational_Resources_Logo.svg" alt="Open Educational Resources Logo" placeholder="blurred" />
                   </div>
                   <div className="column is-12 featured-content ">
                     <h1 className="heading post-category">Research</h1>
-                    <h1 className="title post-title">
-                      Open educational resources (OERs)
-                    </h1>
-                    <p className="post-excerpt">
-                      Open educational resources (OERs) are teaching, learning,
-                      or research materials that are available in the public
-                      domain or released with an intellectual property license
-                      that allows for free use, adaptation, and distribution. We
-                      are currently focusing on developing OERs and their
-                      related technologies.
-                    </p>
+                    <h1 className="title post-title">Open educational resources (OERs)</h1>
+                    <p className="post-excerpt">Open educational resources (OERs) are teaching, learning, or research materials that are available in the public domain or released with an intellectual property license that allows for free use, adaptation, and distribution. We are currently focusing on developing OERs and their related technologies.</p>
                     <br />
-                    <Link to="/en/OER/" className="button">
+                    <Link to="/en/OER/" className="btn">
                       Read More
                     </Link>
                   </div>
@@ -200,7 +135,7 @@ const IndexPageTemplateEN = ({ data }) => (
               <div className="column post is-12">
                 <BlogRoll />
                 <div className="column is-12 has-text-centered">
-                  <Link className="button" to="/news">
+                  <Link className="btn" to="/news">
                     Read more "News and Updates" →
                   </Link>
                 </div>
@@ -222,9 +157,7 @@ const IndexPageTemplateEN = ({ data }) => (
                     <p className="post-excerpt">
                       Professor <br />
                       &nbsp; &nbsp;
-                      <Link to="/en/team/Takaya-Yamazato/">
-                        Takaya YAMAZATO
-                      </Link>
+                      <Link to="/en/team/Takaya-Yamazato/">Takaya YAMAZATO</Link>
                       <br />
                       <br />
                       Secretaries <br />
@@ -284,9 +217,7 @@ const IndexPageTemplateEN = ({ data }) => (
               <div className="column post is-3">
                 <article className="columns is-multiline">
                   <div className="column is-12 featured-content ">
-                    <h1 className="heading post-category">
-                      Undergraduate students
-                    </h1>
+                    <h1 className="heading post-category">Undergraduate students</h1>
                     <h1 className="title post-title">
                       <FaUserFriends />
                     </h1>
@@ -306,7 +237,7 @@ const IndexPageTemplateEN = ({ data }) => (
             <hr />
             <div className="column post is-12">
               <h2 id="contact" className="title post-title">
-                Contact:{' '}
+                Contact:{" "}
               </h2>
 
               <p className="post-excerpt">
@@ -329,57 +260,38 @@ const IndexPageTemplateEN = ({ data }) => (
               <br />
 
               <h3 id="access" className="title post-title">
-                Access:{' '}
+                Access:{" "}
               </h3>
               <p className="post-excerpt">
-                YAMAZATO Laboratory is located at 5th Floor of the North IB
-                Building.
+                YAMAZATO Laboratory is located at 5th Floor of the North IB Building.
                 <br />
-                See also our{' '}
-                <a
-                  href="https://en.nagoya-u.ac.jp/upload_images/20210129_map_en.pdf"
-                  title="Campus map"
-                >
+                See also our{" "}
+                <a href="https://en.nagoya-u.ac.jp/upload_images/20210129_map_en.pdf" title="Campus map">
                   campus map, C3-1
-                </a>{' '}
+                </a>{" "}
                 , for the location of the North IB Building.
               </p>
               <br />
               <h4 className="subtitle post-subtitle">
                 <FaSubway /> From Nagoya Station:
               </h4>
-              <p className="post-excerpt">
-                Take the Subway Higashiyama Line to Motoyama Sta. (15 minutes),
-                then transfer to the Subway Meijo Line to Nagoya Daigaku Sta.
-                (Higashiyama Campus is just off the subway exit.).
-              </p>
+              <p className="post-excerpt">Take the Subway Higashiyama Line to Motoyama Sta. (15 minutes), then transfer to the Subway Meijo Line to Nagoya Daigaku Sta. (Higashiyama Campus is just off the subway exit.).</p>
               <br />
               <h4 className="subtitle post-subtitle">
-                <FaSubway /> From Centrair (Central Japan International
-                Airport):{' '}
+                <FaSubway /> From Centrair (Central Japan International Airport):{" "}
               </h4>
-              <p className="post-excerpt">
-                Take the Meitetsu Line to Kanayama Sta. (30 min.), then transfer
-                to the Subway Meijyo Line to Nagoya Daigaku Sta. (21 min.).
-              </p>
+              <p className="post-excerpt">Take the Meitetsu Line to Kanayama Sta. (30 min.), then transfer to the Subway Meijyo Line to Nagoya Daigaku Sta. (21 min.).</p>
               <br />
               <p className="post-excerpt">
-                <FaSubway /> See also{' '}
-                <a
-                  href="http://en.nagoya-u.ac.jp/access/index.html"
-                  title="Nagoya University HP"
-                >
+                <FaSubway /> See also{" "}
+                <a href="http://en.nagoya-u.ac.jp/access/index.html" title="Nagoya University HP">
                   Nagoya University access map
                 </a>
                 .
               </p>
               <p className="post-excerpt">
                 <SiGooglemaps />
-                &nbsp;See the location of Yamazato laboratory in{' '}
-                <a href="https://www.google.co.jp/maps/place/%E5%90%8D%E5%8F%A4%E5%B1%8B%E5%A4%A7%E5%AD%A6+IB%E9%9B%BB%E5%AD%90%E6%83%85%E5%A0%B1%E9%A4%A8+%E5%8C%97%E6%A3%9F/@35.155147,136.9647537,18z/data=!4m5!3m4!1s0x60037b3dbaffafc5:0x4b259fac77fcc377!8m2!3d35.1553519!4d136.9660213?hl=ja">
-                  Google map
-                </a>
-                .
+                &nbsp;See the location of Yamazato laboratory in <a href="https://www.google.co.jp/maps/place/%E5%90%8D%E5%8F%A4%E5%B1%8B%E5%A4%A7%E5%AD%A6+IB%E9%9B%BB%E5%AD%90%E6%83%85%E5%A0%B1%E9%A4%A8+%E5%8C%97%E6%A3%9F/@35.155147,136.9647537,18z/data=!4m5!3m4!1s0x60037b3dbaffafc5:0x4b259fac77fcc377!8m2!3d35.1553519!4d136.9660213?hl=ja">Google map</a>.
               </p>
 
               {/* <iframe
@@ -398,9 +310,9 @@ const IndexPageTemplateEN = ({ data }) => (
       </div>
     </section>
   </Layout>
-)
+);
 
-export default IndexPageTemplateEN
+export default IndexPageTemplateEN;
 
 export const pageQuery = graphql`
   query IndexPageTemplateEN {
@@ -411,9 +323,7 @@ export const pageQuery = graphql`
       }
       pathPrefix
     }
-    markdownRemark(
-      frontmatter: { templateKey: { eq: "index-page-en" }, lang: { eq: "en" } }
-    ) {
+    markdownRemark(frontmatter: { templateKey: { eq: "index-page-en" }, lang: { eq: "en" } }) {
       id
       frontmatter {
         templateKey
@@ -425,4 +335,4 @@ export const pageQuery = graphql`
       html
     }
   }
-`
+`;
