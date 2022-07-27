8ce8e2c1e36b93d43f266a17e211ddae900f8ce2
Takaya-Yamazato <yamazato@ilas.nagoya-u.ac.jp>
2022-07-27 17:16:16 +0900

    7/27

--- src/pages/inquiry.js
+++ src/pages/inquiry.js
@@ -1,95 +1,35 @@
 import React, { useState } from "react"
-import { withPrefix } from "gatsby";
+// import { useForm, ValidationError } from '@formspree/react';
+// import { withPrefix } from "gatsby";
 import { useForm } from "react-hook-form";
 import styled from "styled-components";
 import Layout from "../components/Layout";
 
-const returnQuestion1 = () => {
-  const question1 = [
-    { value: "このプロジェクトについて" },
-    { value: "公開している教材について" },
-    { value: "技術的な質問" },
-    { value: "より良くするために" },
-  ];
-  return question1;
-}
+// function ContactForm() {
+//   const [state, handleSubmit] = useForm("xjvlnelr");
+//   if (state.succeeded) {
+//       return <p>Thanks for joining!</p>;
+//   }
 
-const returnQuestion2 = () => {
-  const question2 = [
-    { value: "高校・予備校生" },
-    { value: "名古屋大学・大学院の学生" },
-    { value: "名大以外の大学・大学院の学生" },
-    { value: "自主学習者" },
-    { value: "高校・予備校の先生・職員" },
-    { value: "大学・大学院の教職員" },
+const returnQuestion = () => {
+  const question = [
+    { value: "山里研で行っている研究について" },
+    { value: "公開している教材について" },
+    { value: "共同研究，取材のお申し込みについて" },
     { value: "その他" },
   ];
-  return question2;
+  return question;
 }
 
-const returnPrefectures = () => {
-  const prefectures = [
-    { value: "" },
-    { value: "北海道" },
-    { value: "青森" },
-    { value: "岩手" },
-    { value: "宮城" },
-    { value: "秋田" },
-    { value: "山形" },
-    { value: "福島" },
-    { value: "茨城" },
-    { value: "栃木" },
-    { value: "群馬" },
-    { value: "埼玉" },
-    { value: "千葉" },
-    { value: "東京" },
-    { value: "神奈川" },
-    { value: "新潟" },
-    { value: "富山" },
-    { value: "石川" },
-    { value: "福井" },
-    { value: "山梨" },
-    { value: "長野" },
-    { value: "岐阜" },
-    { value: "静岡" },
-    { value: "愛知" },
-    { value: "三重" },
-    { value: "滋賀" },
-    { value: "京都" },
-    { value: "大阪" },
-    { value: "兵庫" },
-    { value: "奈良" },
-    { value: "和歌山" },
-    { value: "鳥取" },
-    { value: "島根" },
-    { value: "岡山" },
-    { value: "広島" },
-    { value: "山口" },
-    { value: "徳島" },
-    { value: "香川" },
-    { value: "愛媛" },
-    { value: "高知" },
-    { value: "福岡" },
-    { value: "佐賀" },
-    { value: "長崎" },
-    { value: "熊本" },
-    { value: "大分" },
-    { value: "宮崎" },
-    { value: "鹿児島" },
-    { value: "沖縄" },
-    { value: "海外" },
-  ];
-  return prefectures;
-}
 
 const Contact = ({ className }) => {
   const [modal, setModal] = useState();
   const [modalOpen, setModalOpen] = useState(false);
   // const { register, handleSubmit, watch, formState: { errors } } = useForm();
   const { register, handleSubmit, formState: { errors } } = useForm();
-  const question1 = returnQuestion1();
-  const question2 = returnQuestion2();
-  const prefectures = returnPrefectures();
+  // const { register, handleSubmit, formState: { errors } } = useForm("xjvlnelr");
+  const question = returnQuestion();
+
   const onSubmit = data => {
     setModalOpen(true);
     const content = (
@@ -109,42 +49,28 @@ const Contact = ({ className }) => {
           </header>
           <section className="modal-card-body">
             <h5>1. ご意見・ご質問の種類を１つ選んでください：</h5>
-            <p>{data.Question1}</p>
-            <h5>2. あなたは：</h5>
-            <p>{data.Question2}</p>
-            <h5>3. お住まいの都道府県：</h5>
-            <p>{data.Prefecture}</p>
-            <h5>4. 件名をご記入下さい：</h5>
+            <p>{data.Question}</p>
+            <h5>2. 件名をご記入下さい：</h5>
             <p>{data.Subject}</p>
-            <h5>5. 内容を具体的にご記入下さい：</h5>
+            <h5>3. 内容を具体的にご記入下さい：</h5>
             <p>{data.Content}</p>
-            <h5>6. お名前：</h5>
+            <h5>4. お名前：</h5>
             <p>{data.Name}</p>
-            <h5>7. 連絡可能なメールアドレス：</h5>
+            <h5>5. 連絡可能なメールアドレス：</h5>
             <p>{data.Email}</p>
           </section>
           <footer className="modal-card-foot">
             <form
               name="contact"
-              // action="https://formspree.io/mqkyenab"
+              action="https://formspree.io/xjvlnelr"
               method="POST"
             >
               <input
                 type="hidden"
                 name="ご意見・ご質問の種類"
-                value={data.Question1}
+                value={data.Question}
               />
-              <input
-                type="hidden"
-                name="あなたは"
-                value={data.Question2}
-              />
-              <input
-                type="hidden"
-                name="お住まいの都道府県"
-                value={data.Prefecture}
-              />
-              <input
+             <input
                 type="hidden"
                 name="件名"
                 value={data.Subject}
@@ -186,59 +112,29 @@ const Contact = ({ className }) => {
     setModal(content);
   }
 
+
   return (
     <div className={className}>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="question-and-form">
           <h3>1. ご意見・ご質問の種類を１つ選んでください：</h3>
           <div className="control">
-            {question1.map(e => (
+            {question.map(e => (
               <div>
                 <label className="radio">
-                  <input {...register("Question1", { required: true })} type="radio" value={e.value} />
+                  <input {...register("Question", { required: true })} type="radio" value={e.value} />
                     {e.value}
                 </label>
               </div>
             ))}
           </div>
-          {errors.Question1 && <span className="has-text-danger">この項目は選択必須です。</span>}
+          {errors.Question && <span className="has-text-danger">この項目は選択必須です。</span>}
         </div>
 
         <div className="question-and-form">
-          <h3>2. あなたは：</h3>
-          <div className="control">
-            {question2.map(e => (
-              <div>
-                <label className="radio">
-                  <input {...register("Question2", { required: true })} type="radio" value={e.value} />
-                    {e.value}
-                </label>
-              </div>
-            ))}
-          </div>
-          {errors.Question2 && <span className="has-text-danger">この項目は選択必須です。</span>}
-        </div>
-
-        <div className="question-and-form">
-          <h3>3. お住まいの都道府県：</h3>
-          <div className="control">
-            <div className="select">
-              <select {...register("Prefecture", { required: true })} >
-                {prefectures.map(e => (
-                  <option value={e.value}>
-                    {e.value ? e.value : "未選択"}
-                  </option>
-                ))}
-              </select>
-            </div>
-          </div>
-          {errors.Prefecture && <span className="has-text-danger">この項目は選択必須です。</span>}
-        </div>
-
-        <div className="question-and-form">
-          <h3>4. 件名をご記入下さい：</h3>
+          <h3>2. 件名をご記入下さい：</h3>
           <p>
-            （例：□□に関する教材も公開してほしい、△△先生の授業教材も公開してほしい、など）
+            （例：□□の研究について詳しく知りたい、△△の教材も公開してほしい、など）
           </p>
           <div className="control columns">
             <div className="column is-half">
@@ -251,11 +147,15 @@ const Contact = ({ className }) => {
         </div>
 
         <div className="question-and-form">
-          <h3>5. 内容を具体的にご記入下さい：</h3>
+          <h3>3. 内容を具体的にご記入下さい：</h3>
           <div className="control columns">
             <div className="column is-half">
               <label className="label">
-                <textarea {...register("Content", {required: true})} className="textarea" />
+                {/* <textarea {...register("Content", {required: true})} className="textarea" /> */}
+                <textarea
+        id="message"
+        name="message"
+      />
               </label>
             </div>
           </div>
@@ -263,7 +163,7 @@ const Contact = ({ className }) => {
         </div>
 
         <div className="question-and-form">
-          <h3>6. お名前：</h3>
+          <h3>4. お名前：</h3>
           <div className="control columns">
             <div className="column is-one-third">
               <label className="label">
@@ -275,11 +175,22 @@ const Contact = ({ className }) => {
         </div>
 
         <div className="question-and-form">
-          <h3>7. 連絡可能なメールアドレス：</h3>
+          <h3>5. 連絡可能なメールアドレス：</h3>
           <div className="control columns">
             <div className="column is-one-third">
               <label className="label">
-                <input {...register("Email", {required: true, pattern: /^[a-zA-Z0-9.!#$%&'*+=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/})} className="input"/>
+                {/* <input {...register("Email", {required: true, pattern: /^[a-zA-Z0-9.!#$%&'*+=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/})} className="input"/> */}
+                <input className="input"
+        id="email"
+        type="email" 
+        name="email"
+        {...register("Email", {required: true, pattern: /^[a-zA-Z0-9.!#$%&'*+=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/})}
+      />
+            {/* <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      /> */}
               </label>
             </div>
           </div>
@@ -305,22 +216,15 @@ const Form = styled(Contact)`
 const FormPage = () => {
   return (
     <Layout>
-      <div
-        className="full-width-image-container margin-top-0"
-        style={{
-          backgroundImage: `url(${withPrefix("/")}img/ITS-VLC.png)`,
-        }}
-      >
-        <h1 className="has-text-weight-bold is-size-1 header-title">
-          お問い合わせ
-        </h1>
-      </div>
       <section className="section section--gradient">
         <div className="container">
           <div className="section">
             <div className="content">
+            <h1 className="has-text-weight-bold is-size-1 header-title">
+          お問い合わせ
+        </h1>
               <p>
-                「名大の授業」についてのお問合せは以下のフォームからお願いします。
+                「山里研究室」についてのお問合せは以下のフォームからお願いします。
               </p>
               <p>ご意見・ご質問をお待ちしております。</p>
               <p>
