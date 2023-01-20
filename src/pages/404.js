cf738de11476f6ffb5519b66bd406edc09cb98b9
Takaya YAMAZATO <yamazato@ilas.nagoya-u.ac.jp>
2023-01-20 21:08:38 +0900

    Update

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,22 @@
-import * as React from 'react'
-import Layout from '../components/Layout'
+import React from "react";
+// import { graphql } from "gatsby"
+import Layout from "../components/Layout";
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
-      <blockquote>
-        <h1 className="has-text-centered">NOT FOUND</h1>
-        <p className="has-text-centered">
-          You just hit a route that doesn&#39;t exist... the sadness.
-        </p>
-      </blockquote>
-    </div>
+    <section className="section section--gradient">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <div className="section">
+              <h1 className="is-size-3 has-text-weight-bold">NOT FOUND</h1>
+              <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
